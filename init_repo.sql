1114dd340a61b18b0892e6e6dc218a7fc5b2fd61
vlkv <vitvlkv@gmail.com>
2010-10-03 23:45:46 +0400

    Изменил имя таблицы object на item

--- init_repo.sql
+++ init_repo.sql
@@ -1,36 +1,38 @@
+-- Пользователь программы
 CREATE TABLE "user" (
-    "name" TEXT PRIMARY KEY NOT NULL,
+    "name" TEXT PRIMARY KEY NOT NULL, -- имя (логин) пользователя
     "password" TEXT NOT NULL, -- может быть пустой пароль, но не NULL
-    "notes" TEXT,
-    "group" TEXT NOT NULL -- Группа определяет права пользователя. Значения: user, admin
+    "notes" TEXT, -- Комментарии, заметки о пользователе, его описание
+    "group" TEXT NOT NULL DEFAULT 'USER' -- Группа определяет права пользователя. Значения: USER, ADMIN
 );
 
--- Объект (или элемент) хранилища данных.
-CREATE TABLE "object" (
+-- Элемент (объект, запись) хранилища данных
+CREATE TABLE "item" (
 	"id" INTEGER PRIMARY KEY NOT NULL, 
 	"name" TEXT NOT NULL, 
 	"notes" TEXT, 
-	"user_name" TEXT,
+	"user_name" TEXT, -- Пользователь-владелец (кто создал элемент)
 	FOREIGN KEY ("user_name") REFERENCES "user"("name")
 );
 
+-- Ссылка на описываемую элементом информацию (данные)
 CREATE TABLE "data" (
-    "uri" TEXT PRIMARY KEY NOT NULL, -- Это путь к файлу относит. корня хранилища, или URL ресурса Интернет --
-    "hash" TEXT, -- хеш от содержимого файла, NULL для url-ссылок --
-    "date_hashed" INTEGER, -- дата/время вычисления хеша --
-    "size" INTEGER NOT NULL DEFAULT 0, -- размер файла в байтах, для url ссылок равно нулю --
-    "order_key" INTEGER, -- ключ для сортировки в пределах одного object-а --
-	"object_id" INTEGER, -- id объекта-владельца. Может быть равен и NULL --
-	"user_name" TEXT, -- пользователь, который добавил файл/url в хранилище. Может быть NULL, тогда это "ничей" файл/url --
-	FOREIGN KEY ("object_id") REFERENCES "object"("id"),
+    "url" TEXT PRIMARY KEY NOT NULL, -- Это путь к файлу относит. корня хранилища, или URL ресурса Интернет
+    "hash" TEXT, -- хеш от содержимого файла, NULL для url-ссылок
+    "date_hashed" INTEGER, -- дата/время вычисления хеша
+    "size" INTEGER NOT NULL DEFAULT 0, -- размер файла в байтах, для url ссылок равно нулю
+    "order_key" INTEGER, -- ключ для сортировки в пределах одного object-а
+	"item_id" INTEGER, -- id объекта-владельца. Может быть равен и NULL
+	"user_name" TEXT, -- пользователь, который добавил файл/url в хранилище. Может быть NULL, тогда это "ничей" файл/url
+	FOREIGN KEY ("item_id") REFERENCES "item"("id"),
 	FOREIGN KEY ("user_name") REFERENCES "user"("name")
 );
-	
--- Тег (или ключевое слово) --
+
+-- Тег (ключевое слово)
 CREATE TABLE "tag" (
-    "name" TEXT NOT NULL,		-- имя тега --
-    "user_name" TEXT NOT NULL,	-- пользователь-владелец тега --
-    "synonym_id" INTEGER,		-- id группы синонимов. Все теги имеющие одинаковые не-NULL значения этого поля считаются синонимами --
+    "name" TEXT NOT NULL,		-- имя тега
+    "user_name" TEXT NOT NULL,	-- пользователь-владелец тега
+    "synonym_id" INTEGER,		-- id группы синонимов. Все теги имеющие одинаковые не-NULL значения этого поля считаются синонимами
     PRIMARY KEY ("name", "user_name"),
 	FOREIGN KEY ("user_name") REFERENCES "user"("name")
 );
@@ -45,28 +47,29 @@ CREATE TABLE "field" (
 	FOREIGN KEY ("user_name") REFERENCES "user"("name")
 );
 
--- Связь между object и field много-ко-многим --
-CREATE TABLE "object_field" (
-    "object_id" TEXT NOT NULL,
+-- Связь между item и field много-ко-многим --
+CREATE TABLE "item_field" (
+    "item_id" TEXT NOT NULL,
     "field_name" TEXT NOT NULL,
     "field_user_name" TEXT NOT NULL,
     "field_value" TEXT NOT NULL,
-	PRIMARY KEY ("object_id", "field_name", "field_user_name"),
-	FOREIGN KEY ("object_id")  REFERENCES "object"("id"),
+	PRIMARY KEY ("item_id", "field_name", "field_user_name"),
+	FOREIGN KEY ("item_id")  REFERENCES "item"("id"),
 	FOREIGN KEY ("field_name", "field_user_name") REFERENCES "field"("name", "user_name")
 );
 
--- Связь между object и tag много-ко-многим --
-CREATE TABLE "object_tag" (
-    "object_id" INTEGER NOT NULL,
+-- Связь между item и tag много-ко-многим --
+CREATE TABLE "item_tag" (
+    "item_id" INTEGER NOT NULL,
     "tag_name" TEXT NOT NULL,
     "tag_user_name" TEXT NOT NULL,
-	PRIMARY KEY ("object_id", "tag_name", "tag_user_name"),
-	FOREIGN KEY ("object_id")  REFERENCES "object"("id"),
+	PRIMARY KEY ("item_id", "tag_name", "tag_user_name"),
+	FOREIGN KEY ("item_id")  REFERENCES "item"("id"),
 	FOREIGN KEY ("tag_name", "tag_user_name") REFERENCES "tag"("name", "user_name")
 );
 
--- Группа тегов/полей. Пользователь может редактировать только свои группы но добавлять в них можно как свои так и чужие теги и поля
+-- Группа тегов/полей. Пользователь может редактировать только свои группы 
+-- но добавлять в них можно как свои так и чужие теги и поля
 CREATE TABLE "bundle" (
     "name" TEXT NOT NULL,
     "user_name" TEXT NOT NULL,
@@ -75,9 +78,9 @@ CREATE TABLE "bundle" (
 	FOREIGN KEY ("user_name") REFERENCES "user"("name")
 );
 
--- Связь между bundle и tag много-ко-многим --
+-- Связь между bundle и tag много-ко-многим
 CREATE TABLE "bundle_tag" (
-    "bundle_name" TEXT NOT NULL,
+    "bundle_name" TEXT NOT NULL,
     "bundle_user_name" TEXT NOT NULL,
     "tag_name" TEXT NOT NULL,
     "tag_user_name" TEXT NOT NULL,
@@ -86,9 +89,9 @@ CREATE TABLE "bundle_tag" (
 	FOREIGN KEY ("tag_name", "tag_user_name") REFERENCES "tag"("name", "user_name")
 );
 
--- Связь между bundle и field много-ко-многим --
+-- Связь между bundle и field много-ко-многим
 CREATE TABLE "bundle_field" (
-    "bundle_name" TEXT NOT NULL,
+    "bundle_name" TEXT NOT NULL,
     "bundle_user_name" TEXT NOT NULL,
     "field_name" TEXT NOT NULL,
     "field_user_name" TEXT NOT NULL,
