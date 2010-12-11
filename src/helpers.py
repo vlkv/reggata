93826159b0e71bbeb81c96fc353eade5c7c93207
vlkv <vitvlkv@gmail.com>
2010-12-12 01:08:14 +0300

    Вынес MyMessageBox наружу

--- src/helpers.py
+++ src/helpers.py
@@ -43,43 +43,43 @@ def tr(text):
     s = QCoreApplication.translate("@default", str(text), None, QCoreApplication.UnicodeUTF8)
     return s
 
-
-def show_exc_info(parent, ex, tracebk=True):
+class MyMessageBox(QtGui.QMessageBox):
     '''Окно данного класса можно растягивать мышкой, в отличие от стандартного 
     QMessageBox-а. Решение взято отсюда: 
     http://stackoverflow.com/questions/2655354/how-to-allow-resizing-of-qmessagebox-in-pyqt4
-    
+    '''
+    def __init__(self, parent=None):
+        super(MyMessageBox, self).__init__(parent)    
+        self.setSizeGripEnabled(True)            
+        #Пока что кнопок еще нет. Они добавятся конечно, когда будет вызван setText(), но пока что их нет
+        self.addButton(QtGui.QMessageBox.Ok)
+        self.setDefaultButton(QtGui.QMessageBox.Ok)
+        self.setEscapeButton(QtGui.QMessageBox.Ok)
+
+    def event(self, e):
+        result = QtGui.QMessageBox.event(self, e)
+
+        self.setMinimumHeight(0)
+        self.setMaximumHeight(16777215)
+        self.setMinimumWidth(0)
+        self.setMaximumWidth(16777215)
+        self.setSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+
+        textEdit = self.findChild(QtGui.QTextEdit)
+        if textEdit != None :
+            textEdit.setMinimumHeight(0)
+            textEdit.setMaximumHeight(16777215)
+            textEdit.setMinimumWidth(0)
+            textEdit.setMaximumWidth(16777215)
+            textEdit.setSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+            
+        return result
+
+def show_exc_info(parent, ex, tracebk=True):
+    '''
     Если traceback задать равным False, то окно не содержит раздел DetailedText
     с информацией о stack trace (как это по русски сказать?).
     '''
-    class MyMessageBox(QtGui.QMessageBox):
-        def __init__(self, parent=None):
-            super(MyMessageBox, self).__init__(parent)    
-            self.setSizeGripEnabled(True)            
-            #Пока что кнопок еще нет. Они добавятся конечно, когда будет вызван setText(), но пока что их нет
-            self.addButton(QtGui.QMessageBox.Ok)
-            self.setDefaultButton(QtGui.QMessageBox.Ok)
-            self.setEscapeButton(QtGui.QMessageBox.Ok)
-    
-        def event(self, e):
-            result = QtGui.QMessageBox.event(self, e)
-    
-            self.setMinimumHeight(0)
-            self.setMaximumHeight(16777215)
-            self.setMinimumWidth(0)
-            self.setMaximumWidth(16777215)
-            self.setSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
-    
-            textEdit = self.findChild(QtGui.QTextEdit)
-            if textEdit != None :
-                textEdit.setMinimumHeight(0)
-                textEdit.setMaximumHeight(16777215)
-                textEdit.setMinimumWidth(0)
-                textEdit.setMaximumWidth(16777215)
-                textEdit.setSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
-                
-            return result
-    
     mb = MyMessageBox(parent)
     mb.setWindowTitle(tr("Information"))
     mb.setText(str(ex))
