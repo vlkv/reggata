5678b125595750a57b4fdd9beaa06425fd9647e1
vlkv <vitvlkv@gmail.com>
2011-04-10 16:19:28 +0400

    Updated start script and version information

--- reggata.bat
+++ reggata.bat
@@ -1,2 +1,6 @@
-set PYTHONPATH=./ui/;./lib/
-python ./src/main_window.py
+rem Please correct your PATH variable value!
+set PATH=c:\usr\Python31_2\Lib\site-packages\PyQt4;c:\usr\Python31_2\Lib\site-packages\PyQt4\bin;"c:\WINDOWS\system32";"c:\WINDOWS";"c:\WINDOWS\system32\wbem";"c:\usr\Python31_2"
+
+set PYTHONPATH=.\ui;.\lib
+
+python .\src\main_window.py
