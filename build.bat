003a54fd88b13bd52252896f7cc6075cef273786
vlkv <vitvlkv@gmail.com>
2012-01-22 18:01:51 +0300

    Fixed build script for Windows.

--- build.bat
+++ build.bat
@@ -1,8 +1,9 @@
 set PYTHON_HOME=c:\usr\Python31_2
+set GIT_BIN=c:\usr\Git\bin
 
 set PATH=%PYTHON_HOME%;%PYTHON_HOME%\Lib\site-packages\PyQt4;%PYTHON_HOME%\Lib\site-packages\PyQt4\bin;c:\WINDOWS\system32;c:\WINDOWS;c:\WINDOWS\system32\wbem;
 
-git describe --tags master > ./version.txt
+%GIT_BIN%\git describe --tags master > ./version.txt
 
 mkdir .\bin
 python .\setup.py build > .\bin\build.out 2>&1
