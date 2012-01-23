76bdacc00251742603e603a6d632dbf87fd67771
unknown <vvolkov@.nwork.local>
2012-01-23 15:45:36 +0400

    Organized run and build scripts in windows

--- build.bat
+++ build.bat
@@ -1,10 +1,6 @@
-set PYTHON_HOME=c:\usr\Python31_2
-set GIT_BIN=c:\usr\Git\bin
-
+call build_set_env.bat
 set PATH=%PYTHON_HOME%;%PYTHON_HOME%\Lib\site-packages\PyQt4;%PYTHON_HOME%\Lib\site-packages\PyQt4\bin;c:\WINDOWS\system32;c:\WINDOWS;c:\WINDOWS\system32\wbem;
-
 %GIT_BIN%\git describe --tags master > ./version.txt
-
 mkdir .\bin
 python .\setup.py build > .\bin\build.out 2>&1
 
--- reggata.bat
+++ reggata.bat
@@ -1,8 +1,5 @@
-rem Change PYTHON_HOME according to your system configuration
-set PYTHON_HOME=c:\usr\Python31_2
-
+call reggata_set_env.bat
 set PATH=%PYTHON_HOME%;%PYTHON_HOME%\Lib\site-packages\PyQt4;%PYTHON_HOME%\Lib\site-packages\PyQt4\bin;c:\WINDOWS\system32;c:\WINDOWS;c:\WINDOWS\system32\wbem;
-
 set PYTHONPATH=.\ui;.\lib
 
 python .\src\reggata.py
