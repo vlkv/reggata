3c5f2b733e1abf537ef439837fd9de0c7ce0b6ee
vlkv <vitvlkv@gmail.com>
2012-01-25 22:04:33 +0300

    TESTS: Corrected module name Issue #12

--- tests/runtests.py
+++ tests/runtests.py
@@ -1,23 +1,23 @@
 import unittest
 import helpers_test
 import repo_mgr_test
+import worker_threads_test
 import tests_context
 import sys
 
 
 if __name__ == '__main__':
-    
+
     # sys.argv[0] contains the name of the executing script
     if (len(sys.argv) >= 2):
         tests_context.TEST_REPO_BASE_PATH = sys.argv[1]
     if (len(sys.argv) >= 3):
         tests_context.COPY_OF_TEST_REPO_BASE_PATH = sys.argv[2]
-    
+
     suite = unittest.TestSuite()
+
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(helpers_test.IsNoneOrEmptyTest))
-    #suite.addTests(unittest.TestLoader().loadTestsFromTestCase(repo_mgr_test.GetItemTest))
-    suite.addTest(repo_mgr_test.GetItemTest('test_getExistingItem'))
-    suite.addTest(repo_mgr_test.GetItemTest('test_getNonExistingItem'))
-    suite.addTest(repo_mgr_test.GetItemTest('test_passBadIdToGetItem'))
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(repo_mgr_test.GetItemTest))
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(worker_threads_test.DeleteGroupOfItemsThreadTest))
+
     unittest.TextTestRunner(verbosity=2).run(suite)
-    
