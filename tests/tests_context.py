ff68d9d7e0b57fd5143d8d0a4d5d18ac638741b8
vlkv <vitvlkv@gmail.com>
2012-04-28 13:58:59 +0300

    TESTS: Added info about new item in test repo

--- tests/tests_context.py
+++ tests/tests_context.py
@@ -11,5 +11,21 @@ existingAliveItem.title = "Don't Forget Me outro jam Montreal 2006"
 existingAliveItem.ownerUserLogin = "user"
 existingAliveItem.relFilePath = "./Dont_Forget_Me_Outro_Jam_Montreal 2006.txt"
 
+# This item id has no corresponding item in test repo
 nonExistingItem = ItemReliableFacts()
 nonExistingItem.id = 1000000000
+
+#This is an existing correct item
+itemWithTagsAndFields = ItemReliableFacts()
+itemWithTagsAndFields.id = 5
+itemWithTagsAndFields.title = "I Could Have Lied.txt"
+itemWithTagsAndFields.relFilePath = "./lyrics/RHCP/I Could Have Lied.txt"
+itemWithTagsAndFields.ownerUserLogin = "user"
+itemWithTagsAndFields.tags = ["RHCP", "Lyrics"]
+itemWithTagsAndFields.fields = {"Rating": 5, 
+                                "Year": 1991, 
+                                "Notes": "This item has both tags and fields",
+                                "Albom": "Blood Sugar Sex Magik"}
+
+
+
