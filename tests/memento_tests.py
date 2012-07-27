76208dbb25d05f6ace9200bf7052aa2149d76e92
vlkv <vitvlkv@gmail.com>
2012-07-27 22:05:08 +0400

    Added stub for ItemSerializationTest

--- tests/memento_tests.py
+++ tests/memento_tests.py
@@ -8,6 +8,9 @@ Created on 27.07.2012
 import unittest
 import db_schema
 import memento
+from tests_context import *
+import os
+from abstract_test_cases import AbstractTestCaseWithRepo
 
 class ItemSerializationSimpleTest(unittest.TestCase):
 
@@ -34,6 +37,17 @@ class ItemSerializationSimpleTest(unittest.TestCase):
         self.assertEquals(item.title, "The Title")
         self.assertIsNone(item.id)
         
+
+class ItemSerializationTest(AbstractTestCaseWithRepo):
+
+    def test_saveExistingItemFromRepo(self):
+        item = self.getExistingItem(itemWithTagsAndFields.id)
+        self.assertEqual(item.title, itemWithTagsAndFields.title)
+        self.assertIsNotNone(item.data_ref)
+        
+        jsonStr = memento.Encoder().encode(item)
+        expectedJsonStr = '{\n    "__class__": "Item", \n    "__module__": "db_schema", \n    "id": 5, \n    "title": "I Could Have Lied.txt"\n}'
+        self.assertEquals(jsonStr, expectedJsonStr)
         
 
 if __name__ == "__main__":
