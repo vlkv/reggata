66708459025af59b79f4343b71260ed679bd6a29
vlkv <vitvlkv@gmail.com>
2012-07-27 21:53:24 +0400

    Simplified a little code of runtests.py

--- tests/runtests.py
+++ tests/runtests.py
@@ -4,10 +4,12 @@ import repo_mgr_test
 import worker_threads_test
 import tests_context
 import sys
+import memento_tests
 
+def addTestCase(suite, testCase):
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(testCase))
 
 if __name__ == '__main__':
-
     # sys.argv[0] contains the name of the executing script
     if (len(sys.argv) >= 2):
         tests_context.TEST_REPO_BASE_PATH = sys.argv[1]
@@ -15,21 +17,16 @@ if __name__ == '__main__':
         tests_context.COPY_OF_TEST_REPO_BASE_PATH = sys.argv[2]
 
     suite = unittest.TestSuite()
+    
+    addTestCase(suite, helpers_test.IsNoneOrEmptyTest)
+    
+    addTestCase(suite, repo_mgr_test.GetItemTest)
+    addTestCase(suite, repo_mgr_test.DeleteItemTest)
+    addTestCase(suite, repo_mgr_test.SaveNewItemTest)
+    addTestCase(suite, repo_mgr_test.UpdateItemTest)
+    
+    addTestCase(suite, worker_threads_test.DeleteGroupOfItemsThreadTest)
+    
+    addTestCase(suite, memento_tests.ItemSerializationSimpleTest)
 
-    
-    
-    
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(helpers_test.IsNoneOrEmptyTest))
-    
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(repo_mgr_test.GetItemTest))
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(repo_mgr_test.DeleteItemTest))
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(repo_mgr_test.SaveNewItemTest))
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(repo_mgr_test.UpdateItemTest))
-    
-    
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(worker_threads_test.DeleteGroupOfItemsThreadTest))
-
-
-    
-    
     unittest.TextTestRunner(verbosity=2).run(suite)
