9c6c1fc2820d7b5c5f202543a0fe6af953de30dc
vlkv <vitvlkv@gmail.com>
2012-08-21 20:17:41 +0400

    Do not show console when starting Reggata

--- reggata.bat
+++ reggata.bat
@@ -2,4 +2,4 @@ call reggata_set_env.bat
 set PATH=%PYTHON_HOME%;%PYTHON_HOME%\Lib\site-packages\PyQt4;%PYTHON_HOME%\Lib\site-packages\PyQt4\bin;c:\WINDOWS\system32;c:\WINDOWS;c:\WINDOWS\system32\wbem;
 set PYTHONPATH=.\ui;.\lib
 
-python .\src\reggata.py
+start pythonw .\src\reggata.py
