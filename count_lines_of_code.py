f98f14b94f4c623e5aca9c092bfbe550778a8a08
Vitaly Volkov <vitvlkv@gmail.com>
2012-09-13 20:55:02 +0000

    Added util script for counting lines of python code

---
+++ count_lines_of_code.py
@@ -0,0 +1,24 @@
+# Prints recursive count of lines of python source code from <current dir>/src
+
+import os
+startPath = os.path.join(os.getcwd(), "src")
+ignore_set = set(["__init__.py"])
+
+resultsList = []
+for pydir, _, pyfiles in os.walk(startPath):
+    for pyfile in pyfiles:
+        if not pyfile.endswith(".py"):
+            continue
+        if pyfile in ignore_set:
+            continue
+        absPath = os.path.join(pydir, pyfile)
+        
+        linesCount = len(open(absPath, "r").read().splitlines())
+        filename = os.path.relpath(absPath, startPath)
+        
+        resultsList.append((linesCount, filename))
+
+for linesCount, filename in resultsList: 
+    print("%d lines in %s" % (linesCount, filename))
+
+print("\nTotal: %s lines (%s)" %(sum([x[0] for x in resultsList]), startPath))
