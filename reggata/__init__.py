df58f5708d84a66a79c6a13908d8aecce63ff768
Panagiotis Mavrogiorgos <pmav99@gmail.com>
2013-01-02 15:55:17 +0200

    Add an __init__.py with info about the project.

---
+++ reggata/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+A tagging system for managing local files on your computer.
+"""
+
+# package information
+__major__ = 0  # for major interface/format changes
+__minor__ = 5  # for minor interface/format changes
+__release__ = 0  # for tweaks, bug-fixes, or development
+__version__ = '%d.%d.%d' % (__major__, __minor__, __release__)
+
+__author__ = "Vitaly Volkov"
+__license__ = "GPL v3+"
+__author_email__ = "vitvlkv@gmail.com"
+__url__ = "https://github.com/vlkv/reggata/wiki"
+__download_url__ = "https://sourceforge.net/projects/reggata/files/"
