bf10cbe6db798dc05a4a35f8019008c4b496e559
vlkv <vitvlkv@gmail.com>
2013-01-09 10:36:22 +0400

    Fixed Windows scripts

--- bin/run_reggata.bat
+++ bin/run_reggata.bat
@@ -3,6 +3,8 @@ set PYTHON_HOME=c:\usr\Python-3.2.2
 
 set PATH=%PYTHON_HOME%;%PYTHON_HOME%\Lib\site-packages\PyQt4;%PYTHON_HOME%\Lib\site-packages\PyQt4\bin;c:\WINDOWS;c:\WINDOWS\system32;c:\WINDOWS\system32\wbem;%PATH%
 
-set PYTHONPATH=%CD%\..;%CD%\..\reggata
+set REGGATA_HOME=%~dp0
 
-start pythonw ../reggata/main.py
+set PYTHONPATH=%REGGATA_HOME%\..;%REGGATA_HOME%\..\reggata
+
+start pythonw %REGGATA_HOME%\..\reggata\main.py
--- bin/run_tests.bat
+++ bin/run_tests.bat
@@ -3,7 +3,10 @@ set PYTHON_HOME=c:\usr\Python-3.2.2
 
 set PATH=%PYTHON_HOME%;%PYTHON_HOME%\Lib\site-packages\PyQt4;%PYTHON_HOME%\Lib\site-packages\PyQt4\bin;c:\WINDOWS;c:\WINDOWS\system32;c:\WINDOWS\system32\wbem;%PATH%
 
-set PYTHONPATH=%CD%\..;%CD%\..\reggata
 
-python ../reggata/tests/runtests.py
+set REGGATA_HOME=%~dp0
+
+set PYTHONPATH=%REGGATA_HOME%\..;%REGGATA_HOME%\..\reggata
+
+python %REGGATA_HOME%\..\reggata\tests\runtests.py
 pause
