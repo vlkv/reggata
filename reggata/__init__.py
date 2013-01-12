81e37c6bb81460099252eb4cc7ca31053f865930
Vitaly Volkov <vitvlkv@gmail.com>
2013-01-12 14:04:57 +0000

    Updated version number for beta release

--- reggata/__init__.py
+++ reggata/__init__.py
@@ -3,10 +3,10 @@
     A tagging system for managing local files on your computer.
 """
 
-__major__ = 0  # for major interface/format changes
-__minor__ = 6  # for minor interface/format changes
-__release__ = 1  # for tweaks, bug-fixes, or development
-__version__ = '%d.%d.%d' % (__major__, __minor__, __release__)
+__major__ = 1  # for major interface/format changes
+__minor__ = 0  # for minor interface/format changes
+__release__ = "0b1"  # for tweaks, bug-fixes, or development
+__version__ = "{}.{}.{}".format(__major__, __minor__, __release__)
 
 __author__ = "Vitaly Volkov"
 __license__ = "GPL v3+"
