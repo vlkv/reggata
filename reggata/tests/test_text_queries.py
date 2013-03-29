74c05674ae7e7d86bd93520d5e16f3713bf3a52b
vlkv <vlkv@bird.home>
2013-03-29 21:23:23 +0400

    QueriesTests has been made extensible

--- reggata/tests/test_text_queries.py
+++ reggata/tests/test_text_queries.py
@@ -2,18 +2,44 @@ from reggata.tests.abstract_test_cases import AbstractTestCaseWithRepo
 from reggata.data.commands import QueryItemsByParseTree
 from reggata.parsers.query_parser import parser
 
-class SimpleQueriesTest(AbstractTestCaseWithRepo):
+
+testsData = [
+    ("Txt AND Lyrics", [14, 15]),
+    ("Txt And Lyrics", [14, 15]),
+    ("Txt and Lyrics", [14, 15]),
+    ("Txt aND Lyrics", []), # 'aND' is interpreted as a name of a tag
+    ("Txt Lyrics", [14, 15]), # AND operator is optional 
+]
+
+
+def create_test_func(query, expectedItemIds):
     
-    def test_Tag1AndTag2(self):
+    def _test_func(self):
         try:
             uow = self.repo.createUnitOfWork()
-            queryTree = parser.parse("Txt AND Lyrics")
+            queryTree = parser.parse(query)
             cmd = QueryItemsByParseTree(queryTree)
             items = uow.executeCommand(cmd)
-            
-            self.assertEqual(len(items), 2)
-            expectedItemIds = [14, 15]
-            self.assertTrue(items[0].id in expectedItemIds)
-            self.assertTrue(items[1].id in expectedItemIds)
+            actualItemIds = [item.id for item in items]
+            actualItemIds.sort()
+            expectedItemIds.sort()
+            self.assertEqual(actualItemIds, expectedItemIds, "Query: {}".format(query))
         finally:
             uow.close()
+            
+    return _test_func
+
+
+class QueriesTests(AbstractTestCaseWithRepo):
+    # NOTE: Test functions will be added to this class dynamically
+    pass
+
+
+# This for cycle dynamically adds test functions to test case class QueriesTests
+for i, (query, expectedItemIds) in enumerate(testsData):
+    setattr(QueriesTests, 'test_{}'.format(i), create_test_func(query, expectedItemIds))
+
+
+
+
+    
