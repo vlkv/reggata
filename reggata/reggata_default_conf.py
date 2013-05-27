367a79a3b52ede44241f300493f9fbb5a4e3c0c7
vitvlkv <vitvlkv@gmail.com>
2013-05-27 22:17:29 +0400

    #34: DONE: IMPROVE: Create different default reggata.conf files for linux and windows

--- reggata/reggata_default_conf.py
+++ reggata/reggata_default_conf.py
@@ -1,8 +1,9 @@
-reggataDefaultConf = \
-'''
+import sys
+
+_minimalReggataConf ='''
+#####################################################################
 # This is a Reggata confguration file ~/.config/reggata/reggata.conf
-#
-#
+#####################################################################
 
 main_window.width=920
 main_window.height=683
@@ -17,47 +18,67 @@ tag_cloud.limit=100
 spinBox_limit.value = 50
 spinBox_limit.page = 1
 
-recent_repo.base_path=/home/vlkv/_reggata_test_repo/
-recent_user.login=vlkv
-recent_user.password=da39a3ee5e6b4b0d3255bfef95601890afd80709
+thumbnail_size = 100
 
-
-#Commands for invoking external applications.
-# %f will be replaced with absolute file name (of selected item)
-# %d will be replaced with absolute path to containing directory (of selected item)
-# If path to command contains spaces, surround it with double quotes,
-# for example:
-# ext_app_mgr.audio.command="C:\Program Files\K-Lite Codec Pack\Media Player Classic\mplayerc.exe" %f
-ext_app_mgr_file_types=['images', 'pdf', 'audio', 'plain_text']
-
-ext_app_mgr.images.extensions=['.jpg', '.png', '.gif', '.bmp']
-ext_app_mgr.images.command="c:\WINDOWS\system32\mspaint.exe" %f
-
-ext_app_mgr.pdf.extensions=['.pdf']
-ext_app_mgr.pdf.command="c:\Program Files\SumatraPDF.exe" %f
-
-ext_app_mgr.audio.extensions=['.mp3', '.ogg', '.flac', '.wav', '.m3u']
-ext_app_mgr.audio.command="c:\Program Files\K-Lite Codec Pack\Media Player Classic\mplayerc.exe" %f
-
-ext_app_mgr.plain_text.extensions=['.txt']
-ext_app_mgr.plain_text.command="c:\WINDOWS\\notepad.exe" %f
-
-#Command for invoking external file manager program
-# %f will be replaced with absolute file name (of selected item)
-# %d will be replaced with absolute path to containing directory (of selected item)
-# If path to command contains spaces, surround it with double quotes
-ext_file_manager="c:\WINDOWS\explorer.exe" %d
+recent_user.login=user
+recent_user.password=12dea96fec20593566ab75692c9949596833adc9
 
 language = en
 
 sqlalchemy.engine_echo = False
 
-thumbnail_size = 100
+'''
 
+_confTemplate = \
+'''
+tmp_dir = {TMP_DIR}
 
-# Default tmp dir is located here ~/.config/reggata/tmp
-# Uncomment only if you want different location of tmp directory
-#tmp_dir = "C:\tmp"
+# Commands for invoking external applications.
+# %f will be replaced with absolute file name (of selected item)
+# %d will be replaced with absolute path to containing directory (of selected item)
+# If path to command contains spaces, surround it with double quotes,
+# for example:
+# ext_app_mgr.audio.command="C:\Program Files\K-Lite Codec Pack\Media Player Classic\mplayerc.exe" %f
+# ext_app_mgr.audio.command=vlc %f
+ext_app_mgr_file_types=['images', 'pdf', 'audio', 'plain_text']
+
+ext_app_mgr.images.extensions=['.jpg', '.png', '.gif', '.bmp']
+ext_app_mgr.images.command={IMAGE_VIEWER} %f
+
+ext_app_mgr.pdf.extensions=['.pdf']
+ext_app_mgr.pdf.command={PDF_VIEWER} %f
+
+ext_app_mgr.audio.extensions=['.mp3', '.ogg', '.flac', '.wav', '.m3u']
+ext_app_mgr.audio.command={AUDIO_PLAYER} %f
+
+ext_app_mgr.plain_text.extensions=['.txt', '.csv']
+ext_app_mgr.plain_text.command={TEXT_EDITOR} %f
+
+# Command for invoking external file manager program
+# %f will be replaced with absolute file name (of selected item)
+# %d will be replaced with absolute path to containing directory (of selected item)
+# If path to command contains spaces, surround it with double quotes
+ext_file_manager={EXTERNAL_FILE_MANAGER} %d
 
 '''
+
+reggataDefaultConf = _minimalReggataConf
+
+if sys.platform.startswith("win"):
+    reggataDefaultConf += _confTemplate.format(
+        IMAGE_VIEWER = '''"c:\WINDOWS\system32\mspaint.exe"''',
+        PDF_VIEWER = '''"c:\Program Files\SumatraPDF.exe"''',
+        AUDIO_PLAYER = '''"c:\Program Files\K-Lite Codec Pack\Media Player Classic\mplayerc.exe"''',
+        TEXT_EDITOR = '''"c:\WINDOWS\\notepad.exe"''',
+        EXTERNAL_FILE_MANAGER = '''"c:\WINDOWS\explorer.exe"''',
+        TMP_DIR = '''"C:\Temp"''')
+
+elif sys.platform.startswith("linux"):
+    reggataDefaultConf += _confTemplate.format(
+        IMAGE_VIEWER = '''gqview''',
+        PDF_VIEWER = '''xpdf''',
+        AUDIO_PLAYER = '''vlc''',
+        TEXT_EDITOR = '''xedit''',
+        EXTERNAL_FILE_MANAGER = '''xfm''',
+        TMP_DIR = '''/tmp''')
 
